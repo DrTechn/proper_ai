19e44915a32a75e5cebebc041f7bcdc650c109f6
Dr Kariem <kariem7175@gmail.com>
2026-07-14 18:04:01 +0300

    Update n8n webhook URL

--- components/N8NChatWidget.tsx
+++ components/N8NChatWidget.tsx
@@ -106,7 +106,7 @@ export default function N8NChatWidget() {
         import { createChat } from "https://cdn.jsdelivr.net/npm/@n8n/chat/dist/chat.bundle.es.js";
 
         createChat({
-          webhookUrl: "https://recolor-hardcopy-curtly.ngrok-free.dev/webhook/024fb15e-9ab3-45f2-8095-1a33ad8d469d/chat",
+          webhookUrl: "https://n8n.kariem.site/webhook/website-chat-trigger/chat",
           defaultLanguage: "ar",
             mode: "window",
            showWelcomeScreen: false,
